ccee994f45d41172ebe944155c7f3c0714f685df
freddy <78168014+freddyjda@users.noreply.github.com>
2025-05-06 20:08:59 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const App = () => {
 
   const startListening = () => {
     SpeechRecognition.startListening({ 
-      language: 'es-ES',
+      language: 'zh-CN',
       continuous: true, // Esto mantiene la escucha activa
       interimResults: false,
       
